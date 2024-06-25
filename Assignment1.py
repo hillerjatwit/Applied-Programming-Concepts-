189dc794bdb727f8bb7c33da07f6381dae93fd09
WITlachr <lachr@wit.edu>
2024-06-25 09:37:17 -0400

    Added course search functionality

--- Assignment1.py
+++ Assignment1.py
@@ -1,5 +1,9 @@
 import class1
-
+import sqlite3
+#from prettytable import PrettyTable
+# database file connection 
+database = sqlite3.connect("assignment3.db") 
+   
 s1 = class1.Student("John", "Doe", 67)
 t1 = class1.Instructor("Mary", "Smith", 23)
 a1 = class1.Admin("Henry", "Wentworth", 2)
@@ -8,8 +12,9 @@ while True:
     match user_in:
         case 1:
             s1.print_all()
-            s1.search_course()
-            s1.add_course()
+            s1.search_all()
+
+            s1.add_course(3226)
             s1.drop_course()
             s1.print_schedule() 
         case 2:
@@ -31,6 +36,39 @@ while True:
             a1.print_courses()
         case 4:
             print("You have exited the program.\n")
+            break
+        case 5:
+            print("You have entered debug mode\n")
+            user_in = int(input("\nEnter a function to test\n1) Search all courses \n2) Search for course with a filter\n"))
+            match user_in:
+                case 1:
+                    s1.search_all()
+                    break
+                case 2:
+                    user_in = int(input("\nEnter a coulumn to filter courses by\n1) CRN\n2) Title\n3) Department\n4) Time\n5) Day of the Week\n6) Year\n7) Credits\n"))
+                    user_in2 = str(input("\nEnter the value to filter: "))
+                    match user_in:
+                        case 1:
+                            user_in3 = "CRN"
+                        case 2:
+                            user_in3 = "TITLE"
+                        case 3:
+                            user_in3 = "DEPARTMENT"
+                        case 4:
+                            user_in3 = "TIME"
+                        case 5:
+                            user_in3 = "DOW"
+                        case 6:
+                            user_in3 = "SEMESTER"
+                        case 7:
+                            user_in3 = "CREDITS"
+                        case _:
+                            user_in3 = "CRN" #default to CRN if invalid input entered
+                            print("Invalid input entered, defaulting to CRN\n")
+
+                    s1.print_course(user_in3, user_in2)
+                    break
+            
             break
         case _:
             print("Enter a valid input\n")
